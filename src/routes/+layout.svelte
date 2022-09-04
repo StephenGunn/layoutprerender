25648bdb2ca83bbd3199ce1da60a5d83384366d0
Stephen <stephengunn@gmail.com>
2022-09-04 13:32:20 -0500

    working

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<nav>
+    <a href="/">home page</a>
+</nav>
+<hr>
+<h2>
+    Select a fake episode:
+</h2>
+<ul>
+    {#each Array(10) as episode, index}
+        <li>
+            <a href="/episodes-{index+1}">Episode #{index+1}</a>
+        </li>
+    {/each}
+</ul>
+<hr>
+<slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Testing layered pre-rendering bug in +layout</h1>
+<p>this repo was made to test a bug in layered pre-rendered layouts.</p>
+<h2>conditions to recreate this bug</h2>
+<ul>
+    <li>
+        set "export const prerender = true" on episodes-[number]/+layout.server.ts
+    </li>
+    <li>
+        deploy to netlify or vercel
+    </li>
+    <li>
+        this bug does not happen when run on localhost through: dev | build | preview - only when deployed
+    </li>
+</ul>
