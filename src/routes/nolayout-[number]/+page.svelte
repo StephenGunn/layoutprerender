2f9fab8216fb11f21103dbbe17338266396d9d6b
Stephen <stephengunn@gmail.com>
2022-09-04 14:18:17 -0500

    again

--- src/routes/nolayout-[number]/+page.svelte
+++ src/routes/nolayout-[number]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import type { PageData } from '../../../.svelte-kit/types/src/routes/nolayout-[number]./../../.svelte-kit/types/src/routes/nolayout-[number]/$types'
-    export let data: LayoutData
+    import type { PageData } from './$types'
+    export let data: PageData
 
 </script>
 
