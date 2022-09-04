79598f55e95c1045f33b4f83c1ba4fb40fd720df
Stephen <stephengunn@gmail.com>
2022-09-04 14:02:48 -0500

    test

--- src/routes/noprerender-[number]/+layout.svelte
+++ src/routes/noprerender-[number]/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import type { LayoutData } from '../../../.svelte-kit/types/src/routes/episode-noprerender-[number]/$types'
+    import type { LayoutData } from './$types'
     export let data: LayoutData
 
 </script>
 
-<h1>Dynamic Test Episode #{data.number} - {data.episode.title} - not pre-rendered at +layout level</h1>
+<h1>Dynamic Test Episode #{data.number} - {data.episode.title} -  pre-render not set at +layout level</h1>
 <nav>
     <a href="/episodes-{data.number}">Episode Home Page</a> | 
     <a href="/episodes-{data.number}/gallery">Episode #{data.number} Gallery</a>
