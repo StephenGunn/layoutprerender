7bc80cd2468dc826ff438093f37d29629ef1d8dd
Stephen <stephengunn@gmail.com>
2022-09-04 14:05:22 -0500

    i think this is ready for review

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,15 @@
 <h2>conditions to recreate this bug</h2>
 <ul>
     <li>
-        set "export const prerender = true" on episodes-[number]/+layout.server.ts
+        set "export const prerender = true" on a /+layout.server.ts file and try to load dynamic data on a sub-route
     </li>
     <li>
         deploy to netlify or vercel
     </li>
     <li>
         this bug does not happen when run on localhost through: dev | build | preview - only when deployed
+    </li>
+    <li>
+        I have setup two versions of the same code. Only thing different is the prerender variable.
     </li>
 </ul>
